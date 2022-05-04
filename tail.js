e073153eca460bcf9594153ddb82d8aae6ca5021
Efe Erkmen <efeerkmen@gmail.com>
2022-05-04 19:28:16 +0000

    Implement tail lotide assignment

---
+++ tail.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(parameter) {
+  let lastValue = parameter [-1]
+  return lastValue
+}
+
+// TEST CODE
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
