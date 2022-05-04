4ed2aa200ab2ecb4a97bae6d0d6279a289861c91
Efe Erkmen <efeerkmen@gmail.com>
2022-05-04 19:23:53 +0000

    Implement head lotide assignment

---
+++ head.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(parameter) {
+  let firstValue = parameter [0]
+  return firstValue
+}
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
