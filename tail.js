0ddfed403dee039e9bf854f8646d023b5f23356e
eerkmen <efeerkmen@gmail.com>
2022-05-26 00:00:00 -0400

    add test code to tail.js file

--- tail.js
+++ tail.js
@@ -11,3 +11,8 @@ const tail = function(parameter) {
   let lastValue = parameter [-1]
   return lastValue
 }
+
+// TEST CODE
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
