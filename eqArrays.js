275aaa1334a634a5368ee4575ac16021437729e1
eerkmen <efeerkmen@gmail.com>
2022-05-27 00:00:00 -0400

    add test code to eqArrays.js file

--- eqArrays.js
+++ eqArrays.js
@@ -13,3 +13,12 @@ const eqArrays = function(array1, array2) {
     return true
   }
 }
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
