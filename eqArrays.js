bc9acb8c0f7712659912c2344a7a789b97a24df0
eerkmen <efeerkmen@gmail.com>
2022-05-27 00:00:00 -0400

    export the eqArrays.js file

--- eqArrays.js
+++ eqArrays.js
@@ -22,3 +22,4 @@ eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays
