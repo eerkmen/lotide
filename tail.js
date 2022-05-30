dcf7f03085e49a55cae583449406c4f64d116fc3
eerkmen <efeerkmen@gmail.com>
2022-05-30 00:00:00 -0400

    remove assertEqual function from tail.js file

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(parameter) {
   let lastValue = parameter [-1];
