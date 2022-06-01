95add06638248334635713db70267585a9f063f3
eerkmen <efeerkmen@gmail.com>
2022-06-01 00:00:00 -0400

    add if statement to eqObjects function in eqObjects.js file

--- eqObjects.js
+++ eqObjects.js
@@ -4,5 +4,9 @@ const eqObjects = function(obj1, obj2) {
   let object1 = Object.keys(obj1);
   let object2 = Object.keys(obj2);
 
-  
+  if (object1.length !== object2.length) {
+    return false;
+  } else {
+    
+  }
 };
