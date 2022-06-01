ae332fb78d9ef2980fc1810be72551eb741cda83
eerkmen <efeerkmen@gmail.com>
2022-06-01 00:00:00 -0400

    add fully functional eqObjects function in eqObjects.js file

--- eqObjects.js
+++ eqObjects.js
@@ -8,7 +8,15 @@ const eqObjects = function(obj1, obj2) {
     return false;
   } else {
     for (let a of object1) {
-     
+      if (object1[a] !== object2[a]) {
+        return false;
+      } else {
+        if (Array.isArray(obj1[a]) && Array.isArray(obj2[a])) {
+          return eqArrays(obj1[a], obj2[a]);
+        } else if (obj1[a] !== obj2[a]) {
+          return false;
+        }
+      }
     }
     return true;
   }
