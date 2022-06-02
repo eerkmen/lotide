b7652688bd9ee1a45ecb676c685e27a863d2251d
eerkmen <efeerkmen@gmail.com>
2022-06-02 00:00:00 -0400

    functional assertObjectsEqual function to assertObjectsEqual.js file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,13 @@
 const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(a, b) {
- 
+  const inspect = require('util').inspect;
+  if (!eqObjects(a, b)) {
+    return console.log(
+      `🛑🛑🛑 Assertion Failed: ${inspect(a)} !== ${inspect(b)}`
+    );
+  }
+  return console.log(
+    `🟢🟢🟢 Asstertion Passed: ${inspect(a)} === ${inspect(b)}`
+  );
 };
