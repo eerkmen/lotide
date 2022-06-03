1994553af2dccd62acf2f38b99438fd2d7a8a721
eerkmen <efeerkmen@gmail.com>
2022-06-03 00:00:00 -0400

    add for loop to countLetters function in countLetters.js file

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,11 @@
 const assertEqual = require('./assertEqual');
 
 const countOnly = function(allItems) {
-  
+  const results = {};
+
+  for (const item of allItems) {
+    // inside the loop:
+   
+  }
+  return results;
 }
