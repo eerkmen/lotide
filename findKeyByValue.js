9552fb50646d64eaed5da24b3dd6f74c70907b6e
eerkmen <efeerkmen@gmail.com>
2022-06-06 00:00:00 -0400

    add findKeyByValue function to findKeyByValue.js file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,10 @@
 const findKeyByValue = function(object, value) {
 
-
+  let endResult = '';
+  for (let a in object) {
+    if (value === object[a]) {
+      endResult = a;
+    }
+  }
+  return endResult;
 };
