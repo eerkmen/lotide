45d8b07aaa01f6dc7e8a7ad18f48bdff2a1ad16f
eerkmen <efeerkmen@gmail.com>
2022-06-06 00:00:00 -0400

    add findKey function to findKey.js file

--- findKey.js
+++ findKey.js
@@ -1,3 +1,10 @@
 const findKey = function(a, b) {
-  
+  let emptyArray = '';
+  for (const i in a) {
+    if (cb(a[i])) {
+      emptyArray = i;
+      return emptyArray;
+    }
+  }
+  return undefined;
 };
