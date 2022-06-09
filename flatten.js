d2f42b5a2487f45d63c697dbcfcbcca702702a88
eerkmen <efeerkmen@gmail.com>
2022-06-09 00:00:00 -0400

    feature:flatten in flatten.js file

--- flatten.js
+++ flatten.js
@@ -1,3 +1,15 @@
 const flatten = function(array) {
 
+  const end = [];
+  for (let i = 0; i < array.length; ++i) {
+    let a = array[i];
+    if (Array.isArray(a)) {
+      for (let k = 0; k < a.length; ++k) {
+        end.push(a[k]);
+      }
+    } else {
+      end.push(a);
+    }
+  }
+  return end;
 };
