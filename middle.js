e7e8214def269b88e3422a3689d2bc72346b770b
eerkmen <efeerkmen@gmail.com>
2022-06-10 00:00:00 -0400

    feature: middle function in middle.js file

--- middle.js
+++ middle.js
@@ -4,5 +4,18 @@ const eqArraysEqual = require('./eqArraysEqual');
 
 // ACTUAL FUNCTION
 const middle = function(arr) {
- 
+  if (arr.length < 3){
+    return value = [];
+  } else if (arr.length % 2 === 0) {
+    let theMiddleFloor = (arr.length / 2) ;
+    let theMiddleCeil = (arr.length / 2) + 1;
+    let value = [theMiddleFloor, theMiddleCeil];
+
+    return value;
+  } else if (arr.length % 2 ===1) {
+    let middle = Math.floor(arr.length / 2);
+    let value = arr[middle]
+    return value
+  }
+  
 }
