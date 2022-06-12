6537f2b6b3aeb9c7756ec8e8ded022efe3f5eba2
eerkmen <efeerkmen@gmail.com>
2022-06-12 00:00:00 -0400

    feature:takeuntil function to takeUntil.js file

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,11 @@
 const takeUntil = function(a, b) {
-  
+  const end = [];
+  for (let i of a) {
+    if (!callback(i)) {
+      break;
+    } else {
+      final.push(i);
+    }
+  }
+  return end;
 };
