089927036619ee5d0aea9043d5cd67ce21bb1218
eerkmen <efeerkmen@gmail.com>
2022-06-13 00:00:00 -0400

    feature:letterPositions function to letterPositions.js file

--- letterPositions.js
+++ letterPositions.js
@@ -4,6 +4,17 @@ const assertArraysEqual = require('./assertArraysEqual');
 
 
 const letterPositions = function(allItems) {
-  
+  const results = {};
+
+  for (const item of allItems) {
+    // inside the loop:
+   
+    if (results[item]) {
+      results[item] += allItems[item];
+    } else {
+      results[item] = allItems[item];
+    }
+  }
+  return results;
 };
 
