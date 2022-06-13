b4765fc3e17384abb1fe97e6e67a2df5045263d8
eerkmen <efeerkmen@gmail.com>
2022-06-13 00:00:00 -0400

    feature: without function to without.js file

--- without.js
+++ without.js
@@ -22,5 +22,11 @@ const eqArrays = function(array1, array2) {
 }
 
 const without = function(source, itemsToRemove) {
- 
+  const result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i]) && !result.includes(source[i])) {
+      result.push(source[i]);
+    }
+  }
+  return result;
 };
