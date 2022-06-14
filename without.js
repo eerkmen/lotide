51f8137ee00b843887b49d409b5fd740eb16907e
eerkmen <efeerkmen@gmail.com>
2022-06-14 00:00:00 -0400

    add test code to without.js file

--- without.js
+++ without.js
@@ -33,3 +33,9 @@ const without = function(source, itemsToRemove) {
 
 
 module.exports = without;
+// TEST CODE
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
