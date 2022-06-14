57fa904d5866546f0b037c064418e60a88871d40
eerkmen <efeerkmen@gmail.com>
2022-06-14 00:00:00 -0400

    add require functions to index.js file

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const eqObjects = require('./eqObjects');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const flatten = require('./flatten');
+const findKey = require("./findKey");
+const findKeyByValue = require('./findKeyByValue');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+
+
+
+
