6ef5e3e2c8d9c8ab5cff477c8790bd6c88745298
eerkmen <efeerkmen@gmail.com>
2022-06-16 00:00:00 -0400

    copy eqArrays to test/eqArraysTest.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = require('../assertEqual');
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  } else {
+    for (var i=0; i < array1.length; i++) {
+      if (array1[i] != array2[i]) {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays
