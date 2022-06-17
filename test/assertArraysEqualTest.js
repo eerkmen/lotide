98f362ff66be14430fc9b6ab17adf93e418a250c
eerkmen <efeerkmen@gmail.com>
2022-06-17 00:00:00 -0400

    change require route in test/assertArraysEqualTest.js

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = require('./eqArrays');
+const eqArrays = require('../eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
